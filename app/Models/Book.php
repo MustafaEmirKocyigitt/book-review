760eaaa4ee3064c17453f8576df2ae5d3a21bd52
MustafaEmirKocyigitt <mustafaemirkocyigit@gmail.com>
2024-04-23 12:39:24 +0300

    Popular or Highest Rated - The Logic && The syntax errors have been corrected.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -22,30 +22,55 @@ class Book extends Model
     public function scopePopular(Builder $query,$from = null , $to = null):Builder | QueryBuilder
     {
         return $query->withCount([
-            'reviews' =>fn(Builder $q)=>$this->dataRangeFilter($q,$from,$to)
+            'reviews' =>fn(Builder $q)=>$this->dateRangeFilter($q,$from,$to)
         ])
             ->orderByDesc('reviews_count');
     }
-    public function scopeHighestRated(Builder $query):Builder | QueryBuilder
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
         return $query->withAvg([
-            'reviews' =>fn(Builder $q)=>$this->dataRangeFilter($q,$from,$to)
-        ],'rating')
-        ->orderByDesc('reviews_avg_rating');
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
     }
+
 
     public function scopeMinReviews(Builder $query,int $minReviews):Builder | QueryBuilder
     {
         return $query->having('reviews_count','>=',$minReviews);
     }
-    private function dataRangeFilter(Builder $query,$from = null , $to = null):Builder | QueryBuilder
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
     {
-        if($from && $to){
-            $query->where('created_at','>=',$from); 
-        }elseif(!$from && $to){
-            $query->where('created_at','<=',$to);
-        }elseif($from && $to){
-            $query->whereBetween('created_at',[$from,$to]);
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
         }
+    }
+    public function scopePopularLastMonth(Builder $query):Builder | QueryBuilder
+    {
+        return $query->popular(now()->subMonth(),now())
+        ->highestRated(now()->subMonth(),now())
+        ->minReviews(2);
+    }
+    public function scopePopularLast6Months(Builder $query):Builder | QueryBuilder
+    {
+        return $query->popular(now()->subMonths(6),now())
+        ->highestRated(now()->subMonths(6),now())
+        ->minReviews(5);
+    }
+    public function scopeHighestRatedLastMonth(Builder $query):Builder | QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(),now())
+        ->popular(now()->subMonth(),now())
+        ->minReviews(2);
+    }
+    public function scopeHighestRatedLast6Months(Builder $query):Builder | QueryBuilder
+    {
+        return $query->highestRated(now()->subMonths(6),now())
+        ->popular(now()->subMonths(6),now())
+        ->minReviews(5);
     }
 }
